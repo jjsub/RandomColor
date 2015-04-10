e89ef8eeddffd6d87d780c1356219032232ec46e
jjsub <jsamesub@gmail.com>
2015-04-10 14:20:42 -0500

    loop set up to rep after chroma array

--- colorBoxes/script.js
+++ colorBoxes/script.js
@@ -2,12 +2,24 @@
 
 var element     = document.querySelectAll(""),
     chroma      = [],
-    nowChroma  = [];
+    nowChroma   = [],
+    i;
 
     setTimeout(function(){
+      
+      for(i = 0; i < element.length; i ++){
+          if( i == element.length-1){
+              nowChroma[i] = chroma[0];
+              chroma       = nowChroma;
+          }
 
+          else{
 
+              nowChroma[i] = chroma[i+1];
 
-
+          }
+          
+        element[i].style.color = nowChroma[i];
+      }
 
     }, 1000);
